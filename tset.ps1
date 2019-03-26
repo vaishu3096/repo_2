d10e34863bdf633a194c72e51b310cc2094fb61f
vaishu3096 <48466978+vaishu3096@users.noreply.github.com>
2019-03-26 11:00:29 +0530

    Update tset.ps1

--- tset.ps1
+++ tset.ps1
@@ -1,3 +1,3 @@
-$q1 = ("C:\Users\vaishnavi.ar\Desktop\testfolder\test.sql")
+$q1 = ("https://github.com/vaishu3096/repo_2/blob/master/test.sql")
 echo $q1
 Invoke-Sqlcmd -InputFile "$q1"  -ServerInstance "CHNMCT306796D\SQLEXPRESS" -database "master"
